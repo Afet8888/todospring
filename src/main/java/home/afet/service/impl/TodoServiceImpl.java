12b347a7ed66bf8dde4cbdaef2d8f8acb6d727bb
Afet8888 <baku.9999@hotmail.com>
2018-04-08 15:20:04 +0400

    More fixes

--- src/main/java/home/afet/service/impl/TodoServiceImpl.java
+++ src/main/java/home/afet/service/impl/TodoServiceImpl.java
@@ -27,43 +27,49 @@ public class TodoServiceImpl implements TodoService {
 
     @Override
     public boolean post(Todo todo) {
-            if(todos.parallelStream().anyMatch(el->el.getId() == todo.getId()))return false;
-            todos.add(todo);
-            return true;
-    }
-
-    @Override
-    public Todo get(int id) {
-        if(todos.parallelStream().anyMatch(el->el.getId() == id))
-        return todos.parallelStream().filter(elem -> elem.getId() == id).findAny().get();
-        else  return null;
-    }
-
-    @Override
-    public boolean delete(int id) {
-        Todo tempTodo = todos.stream().filter(elem -> elem.getId() == id).findAny().get();
-        if (Objects.isNull(tempTodo)) {
-            return false;
-        } else
-            todos.remove(tempTodo);
+        if (todos.parallelStream().anyMatch(el -> el.getId() == todo.getId())) return false;
+        todos.add(todo);
         return true;
     }
 
     @Override
-    public boolean put(Todo todo) {
-        Todo tempTodo = todos.stream().filter(elem -> elem.getId() == todo.getId()).findAny().get();
-        if (Objects.isNull(tempTodo)) {
-            return false;
-        } else {
-            tempTodo.setDate(todo.getDate());
-            tempTodo.setText(todo.getText());
-            tempTodo.setTitle(todo.getTitle());
-            tempTodo.setDone(todo.isDone());
+    public Todo get(int id) {
+        if (todos.parallelStream().anyMatch(el -> el.getId() == id))
+            return todos.parallelStream().filter(elem -> elem.getId() == id).findAny().get();
+        else return null;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        if (todos.parallelStream().noneMatch(el -> el.getId() == id)) return false;
+        else {
+            todos.removeIf(el -> el.getId() == id);
+            //todos.remove(todos.stream().filter(el->el.getId() == id).findAny().get());
             return true;
         }
     }
-        @Autowired
-        public void setTodos (List < Todo > todos) {
-            this.todos = todos;
-        }
+
+    @Override
+    public boolean put(Todo todo) {
+        if (findIndex(todo.getId()) == -1) return false;
+        Todo tempTodo = todos.stream().filter(elem -> elem.getId() == todo.getId()).findAny().get();
+        tempTodo.setDate(todo.getDate());
+        tempTodo.setText(todo.getText());
+        tempTodo.setTitle(todo.getTitle());
+        tempTodo.setDone(todo.isDone());
+        return true;
     }
+
+    @Autowired
+    public void setTodos(List<Todo> todos) {
+        this.todos = todos;
+    }
+
+    int findIndex(int id) {
+
+        for (int i = 0; i < todos.size(); i++) {
+            if (todos.get(i).getId() == id) return i;
+        }
+        return -1;
+    }
+}
